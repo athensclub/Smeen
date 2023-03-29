dc7c0666021634a5b6c54e329354ad884fc0fa0f
athensclub <43963760+athensclub@users.noreply.github.com>
2023-03-29 14:33:48 +0700

    edit: more font types

--- src/smeen/global/Fonts.java
+++ src/smeen/global/Fonts.java
@@ -11,9 +11,15 @@ public final class Fonts {
 
     public static final Font BASIC_BOLD_FONT;
 
+    public static final Font SMALL_REGULAR_FONT;
+
+    public static final Font EXTRA_SMALL_REGULAR_FONT;
+
     static{
         BASIC_BOLD_FONT = Font.loadFont(Fonts.class.getResourceAsStream("/fonts/IBMPlexSansThai-Bold.ttf"), 24);
         BASIC_REGULAR_FONT = Font.loadFont(Fonts.class.getResourceAsStream("/fonts/IBMPlexSansThai-Regular.ttf"), 24);
+        SMALL_REGULAR_FONT  = Font.loadFont(Fonts.class.getResourceAsStream("/fonts/IBMPlexSansThai-Bold.ttf"), 14);
+        EXTRA_SMALL_REGULAR_FONT = Font.loadFont(Fonts.class.getResourceAsStream("/fonts/IBMPlexSansThai-Bold.ttf"), 10);
         THAI_FONT_FAMILY = BASIC_BOLD_FONT.getFamily();
     }
 
