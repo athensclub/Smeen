e44abda57a260b26b993db669ac0180b930329df
athensclub <43963760+athensclub@users.noreply.github.com>
2023-03-29 14:34:34 +0700

    feat: CodeBlock draggable from CodeSelector

--- src/smeen/component/code/CodeSelector.java
+++ src/smeen/component/code/CodeSelector.java
@@ -4,17 +4,21 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import smeen.component.code.block.movement.MoveCodeBlock;
 import smeen.global.Fonts;
+import smeen.views.MainView;
 
 public class CodeSelector extends ScrollPane {
 
-    public CodeSelector(){
+    public CodeSelector(MainView main){
         VBox root = new VBox();
 
         Label moveLabel = new Label("เคลื่อนไหว");
         moveLabel.setFont(Fonts.BASIC_BOLD_FONT);
 
-        root.getChildren().addAll(moveLabel);
+        CodeBlock move = new MoveCodeBlock(main);
+
+        root.getChildren().addAll(moveLabel, move);
 
         setContent(root);
     }
--- src/smeen/views/MainView.java
+++ src/smeen/views/MainView.java
@@ -1,23 +1,39 @@
 package smeen.views;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.event.ActionEvent;
+import javafx.geometry.Point2D;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import smeen.component.code.CodeBlock;
 import smeen.component.code.CodeSelector;
 
 
-public class MainView extends VBox {
-
+public class MainView extends Pane {
     private final MenuBar menuBar;
     private final Menu fileMenu;
-
     private final CodeSelector codeSelector;
+    private final ObjectProperty<CodeBlock> draggingBlock;
 
-    public MainView(){
+    public MainView() {
+        draggingBlock = new SimpleObjectProperty<>();
+        draggingBlock.addListener((obs, oldv, newv) -> {
+            getChildren().remove(oldv);
+            if (newv != null) {
+                getChildren().add(newv);
+            }
+        });
+
         setMinSize(600, 400);
+
+        VBox root = new VBox();
+        root.prefWidthProperty().bind(widthProperty());
+        root.prefHeightProperty().bind(heightProperty());
 
         fileMenu = new Menu("File");
 
@@ -29,9 +45,43 @@ public class MainView extends VBox {
 
         menuBar = new MenuBar(fileMenu);
 
-        codeSelector = new CodeSelector();
+        codeSelector = new CodeSelector(this);
 
-        getChildren().addAll(menuBar, codeSelector);
+        root.getChildren().addAll(menuBar, codeSelector);
+
+        getChildren().addAll(root);
+
+        final double[] oldPos = {-1, -1}; // oldPos[0] = x, oldPos[1] = y
+        final boolean[] dragging = { false };
+        addEventFilter(MouseEvent.MOUSE_DRAGGED, e -> {
+            if(draggingBlock.get() == null)
+                return;
+
+            if(!dragging[0]){
+                oldPos[0] = e.getX();
+                oldPos[1] = e.getY();
+                dragging[0] = true;
+                return;
+            }
+
+            double dx = e.getX() - oldPos[0];
+            double dy = e.getY() - oldPos[1];
+
+            CodeBlock block = draggingBlock.get();
+            block.relocate(block.getLayoutX() + dx, block.getLayoutY() + dy);
+
+            oldPos[0] = e.getX();
+            oldPos[1] = e.getY();
+        });
+
+        addEventFilter(MouseEvent.MOUSE_RELEASED, e -> {
+            dragging[0] = false;
+            draggingBlock.set(null);
+        });
+    }
+
+    public ObjectProperty<CodeBlock> draggingBlockProperty() {
+        return draggingBlock;
     }
 
 }
