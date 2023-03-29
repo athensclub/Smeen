72db48675b8d4016ecae27f56f318e5669df436c
athensclub <43963760+athensclub@users.noreply.github.com>
2023-03-29 14:34:20 +0700

    add: MoveCodeBlock component

---
+++ src/smeen/component/code/block/movement/MoveCodeBlock.java
@@ -0,0 +1,39 @@
+package smeen.component.code.block.movement;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.paint.Color;
+import javafx.util.converter.IntegerStringConverter;
+import smeen.component.code.CodeBlock;
+import smeen.global.Fonts;
+import smeen.views.MainView;
+
+public class MoveCodeBlock extends CodeBlock {
+
+    private TextField amount;
+
+    public MoveCodeBlock(MainView main){
+        super(main);
+        Label first = new Label("เคลื่อนที่");
+        first.setTextFill(Color.WHITE);
+        first.setFont(Fonts.SMALL_REGULAR_FONT);
+
+        amount = new TextField();
+        amount.setPrefWidth(30);
+        amount.setPrefHeight(20);
+        amount.setTextFormatter(new TextFormatter<>(new IntegerStringConverter()));
+        amount.setFont(Fonts.EXTRA_SMALL_REGULAR_FONT);
+
+        Label second = new Label("ก้าว");
+        second.setTextFill(Color.WHITE);
+        second.setFont(Fonts.SMALL_REGULAR_FONT);
+
+        getChildren().addAll(first, amount, second);
+    }
+
+    @Override
+    public CodeBlock copy() {
+        return new MoveCodeBlock(getMain());
+    }
+}
