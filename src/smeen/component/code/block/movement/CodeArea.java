f935b71c656c531bcb890fc9e25b75c24524ba7e
athensclub <43963760+athensclub@users.noreply.github.com>
2023-03-29 15:38:28 +0700

    add: CodeArea component

---
+++ src/smeen/component/code/block/movement/CodeArea.java
@@ -0,0 +1,17 @@
+package smeen.component.code.block.movement;
+
+import javafx.geometry.Point2D;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import smeen.component.code.CodeBlock;
+import smeen.views.MainView;
+
+public class CodeArea extends Pane {
+
+    public CodeArea(MainView main){
+        setBackground(Background.fill(Color.WHITE));
+    }
+
+}
