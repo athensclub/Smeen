c3af3e6be6bf3915f0345271d7a00fbb1d3bfae1
athensclub <43963760+athensclub@users.noreply.github.com>
2023-04-03 18:23:04 +0700

    add: Snap Threshold constant

---
+++ src/smeen/global/SmeenConstants.java
@@ -0,0 +1,10 @@
+package smeen.global;
+
+public class SmeenConstants {
+
+    /**
+     * The distance (in pixels) that two CodeBlockList can have at most to be able to snap.
+     */
+    public static final double SNAP_THRESHOLD = 15;
+
+}
